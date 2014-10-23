a16a5954b564aafbb833ea20bfd8a1b8cdab139b
Pablo Morales Alarcón <pamoral@alumnos.ubiobio.cl>
2014-10-22 23:58:35 -0300

    Revert "Fin restaurantLayout"
    This reverts commit 1f159ecad63d01f7d9c1749c123f76a91bb5f8f4.

--- protected/views/layouts/restaurantLayout.php
+++ protected/views/layouts/restaurantLayout.php
@@ -38,29 +38,38 @@
         <li><a href="#">Restaurant</a></li>
         <li><a href="#">Garzón</a></li>
         <li><a href="#">Proveedor</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+            <li class="divider"></li>
+            <li><a href="#">One more separated link</a></li>
+          </ul>
+        </li>
       </ul>
-      <!-- Busqueda -->
       <form class="navbar-form navbar-left" role="search">
         <div class="form-group">
           <input type="text" class="form-control" placeholder="Search">
         </div>
         <button type="submit" class="btn btn-default">Submit</button>
       </form>
-      <!-- Termino busqueda -->
-
-      <!-- Lado derecho Logout -->
       <ul class="nav navbar-nav navbar-right">
-            <li><a>Bienvenido <?php if(isset(Yii::app()->user->nombre))echo Yii::app()->user->nombre; ?></a></li>
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-cog"></span></a>
-              <ul class="dropdown-menu">
-                <li><a href="<?php echo Yii::app()->createUrl('Usuario/editarUsuario/'.Yii::app()->user->ID); ?>">Editar Perfil</a></li>
-                <li class="divider"></li>
-                <li><a href="<?php echo Yii::app()->createUrl('Usuario/logout'); ?>">Cerrar Sesión</a></li>
-              </ul>
-            </li>
+        <li><a href="#">Link</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li class="divider"></li>
+            <li><a href="#">Separated link</a></li>
           </ul>
-
+        </li>
+      </ul>
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
 </nav>
